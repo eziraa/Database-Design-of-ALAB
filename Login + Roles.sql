3145690ab4a2aa67c2785998ec895ad78b9f3e6a
eziraa <ezratgab@gmail.com>
2024-01-31 02:47:42 +0300

    login and roles created

--- Login + Roles.sql
+++ Login + Roles.sql
@@ -36,3 +36,51 @@ CREATE USER Nardos FOR LOGIN Nardos
 CREATE LOGIN Kefyalew WITH PASSWORD = '12345'
 CREATE USER Kefyalew FOR LOGIN Kefyalew
 
+
+-- CREATING ROLES
+
+-- Creating role for Project manager
+CREATE ROLE systemAdmins
+sp_addrolemember systemAdmins, Ezira
+
+-- Creating role for Project manager
+CREATE ROLE projectManagers
+	sp_addrolemember projectManagers, Afework
+
+-- Creating role for Land Owner
+CREATE ROLE landOwners
+	sp_addrolemember landOwners, Girma
+	sp_addrolemember landOwners, Dawit
+
+
+-- Creating role for Notifier
+CREATE ROLE notifiers
+sp_addrolemember notifiers, Girma
+
+
+-- Creating role for Minute Document Holder
+CREATE ROLE minDocHolders
+sp_addrolemember minDocHolders, Nardos
+
+
+-- Creating role for Property Counter
+CREATE ROLE propCounters
+sp_addrolemember propCounters, Tamirat
+
+
+
+-- Creating role for Estimator
+CREATE ROLE estimators
+sp_addrolemember estimators, Kefyalew
+
+
+
+-- Creating role for Rehabilitator
+CREATE ROLE rehabilitators
+sp_addrolemember rehabilitators, Dawud
+
+
+-- Creating role for Payment checker
+CREATE ROLE payCheckers
+sp_addrolemember payCheckers, payChecker
+
