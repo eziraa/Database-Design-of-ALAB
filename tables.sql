734c6e1debeb87f678bc1e5fbf744070ddb57fab
eziraa <ezratgab@gmail.com>
2024-01-30 17:13:19 +0300

    table landOwner created

--- tables.sql
+++ tables.sql
@@ -6,3 +6,15 @@ CREATE TABLE tblAddress (
 	[Wereda] VARCHAR(23),
 	[Kebele] VARCHAR(23),
 	[Sub Kebele] VARCHAR(23) CONSTRAINT tblAddressPK PRIMARY KEY )
+
+
+--CREATE LAND OWNER TABLE 
+CREATE TABLE tblLandOwner(
+	[Land Owner ID] INT CONSTRAINT tblLandOwnerPK PRIMARY KEY,
+	[First Name] VARCHAR(23),
+	[Last Name] VARCHAR (23),
+	[Gender] CHAR,
+	[Birth Date] DATE,
+	[Phone Number] INT UNIQUE,
+	[Photo] VARBINARY(MAX),
+	[Sub Kebele] VARCHAR (23) CONSTRAINT tblLandOwnerFK1 FOREIGN KEY REFERENCES LandOwner.tblAddress([Sub Kebele]))
