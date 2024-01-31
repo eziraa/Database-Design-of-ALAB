491d5e78974cd66407e40a5b0ec006e3fbe1b6b1
eziraa <ezratgab@gmail.com>
2024-01-31 18:49:25 +0300

    Store Procedure spMakeRequest created to make request

--- Procedure.sql
+++ Procedure.sql
@@ -25,3 +25,55 @@ BEGIN
 END 
 GO
 
+
+
+--THE PROJECT MANAGER MAKE Land request
+
+GO
+CREATE PROCEDURE Request.spMakeRequest (
+    @projectType VARCHAR(23),
+    @Urgency VARCHAR(12),
+    @LandRecoverability VARCHAR(12), 
+    @RequestedArea FLOAT,
+    @ProjectDuration FLOAT,
+    @SubKebele VARCHAR(23),
+    @ProjectName VARCHAR(23)
+)
+AS 
+BEGIN TRANSACTION T1
+    DECLARE @PRID INT
+    IF NOT EXISTS (SELECT [Project ID] FROM Request.tblProject WHERE [Project Name] = @ProjectName)
+        INSERT INTO Request.tblProject VALUES(@ProjectName, @projectType)
+        
+    SELECT  @PRID = (SELECT [Project ID] FROM Request.tblProject WHERE [Project Name] = @ProjectName)
+
+    IF EXISTS (SELECT * FROM Request.tblProReqToLand WHERE [Project ID] = @PRID AND [Responsed By] IS NOT NULL) 
+        OR	NOT  EXISTS (SELECT * FROM Request.tblProReqToLand WHERE [Project ID] = @PRID)
+    
+        INSERT INTO Request.tblProReqToLand (
+            [Urgency],
+            [Land Recoverability],
+            [Request Date],
+            [Requested Area],
+            [Project Duration],
+            [Sub Kebele],
+            [Project ID]
+        ) VALUES (
+            @Urgency,
+            @LandRecoverability,
+            GETDATE(),
+            @RequestedArea,
+            @ProjectDuration,
+            @SubKebele,
+            @PRID
+        )
+    
+    ELSE 
+        RAISERROR('The request already exists', 16, 1)
+
+    IF (@@ERROR <> 0)
+        ROLLBACK TRANSACTION T1
+        COMMIT 
+
+GO
+
