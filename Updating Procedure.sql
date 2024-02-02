3051eece02e69e77152903a4c7d3a072714eab61
eziraa <ezratgab@gmail.com>
2024-02-02 14:43:08 +0300

    Adding procdure to update non productive Plant table

--- Updating Procedure.sql
+++ Updating Procedure.sql
@@ -11,3 +11,19 @@ BEGIN
 		UPDATE Property.tblLandGivesCrop SET [Hervest QPerH Two Year Ago] = @UpdateValue WHERE [Land ID] = @landID AND [Crop Name] = @cropName
 END
 GO
+
+
+-- to update non productive plants table
+
+GO
+CREATE PROCEDURE Compensation.spUpdate2LandGrowsNonProPlant(@Name VARCHAR(23), @landID INT, @UpdateValue INT, @UpdateType VARCHAR(23))
+AS
+BEGIN
+	IF(@UpdateType = 'quantity')
+		UPDATE Property.tblLandGrowsNonProPlants SET [Quantity] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+	ELSE IF(@UpdateType = 'growth')
+		UPDATE Property.tblLandGrowsNonProPlants SET [Growth Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+	ELSE 
+		UPDATE Property.tblLandGrowsNonProPlants SET [Preservation Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+END
+GO
