2f3ed55408dac5fa6df5ec897142524fe50b4d4a
eziraa <ezratgab@gmail.com>
2024-02-02 15:14:45 +0300

    crating trigger to update total compensation  when productive plant updated

--- Triggers.sql
+++ Triggers.sql
@@ -27,3 +27,18 @@ IF(UPDATE ([Quantity])) OR (UPDATE ([Growth Expense])) OR (UPDATE ([Preservation
 END
 GO
 
+
+-- create trigger to update the total comopensation when  productive plant quantity is updated
+GO
+CREATE TRIGGER Property.trigUpdateTotalByPro ON Property.tblLandGrowsProdPlants 
+AFTER UPDATE
+AS
+IF(UPDATE ([Growth Expense]) OR UPDATE ([Preservation Expense]) OR UPDATE ([High Level Quantity]) OR UPDATE( [Middle Level Quantity])
+OR UPDATE ([Low Level Quantity]))
+BEGIN
+  DECLARE @temp1 INT ,@temp2 INT
+  SELECT @temp1 = (SELECT Compensation.fnTotalComp ([Land ID]) FROM INSERTED)
+  SELECT @temp2 = ( SELECT [Land Owner ID] from Property.tblLand WHERE [Land ID] = (SELECT [Land ID] FROM INSERTED))
+  UPDATE Compensation.tblTotalCompensation SET [Amount] = @temp1 WHERE [Land Owner ID] = @temp2
+END
+GO
