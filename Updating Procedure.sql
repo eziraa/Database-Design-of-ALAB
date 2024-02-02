6d6de2dc385d4aa1801399ae08ae2f60dc4a4039
eziraa <ezratgab@gmail.com>
2024-02-02 14:45:43 +0300

    adding proceure to update movable property table

--- Updating Procedure.sql
+++ Updating Procedure.sql
@@ -27,3 +27,38 @@ BEGIN
 		UPDATE Property.tblLandGrowsNonProPlants SET [Preservation Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
 END
 GO
+
+-- TO UPDATE LANG GROWS PRODUCTIVE PLANT TABLE
+
+GO
+CREATE PROCEDURE Compensation.spUpdate2LandGrowsProdPlant(@Name VARCHAR(23), @landID INT, @UpdateValue INT, @UpdateType VARCHAR(23))
+AS
+BEGIN
+	IF(@UpdateType = ' low')
+		UPDATE Property.tblLandGrowsProdPlants SET [Low Level Quantity] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+	ELSE IF(@UpdateType = 'mid')
+		UPDATE Property.tblLandGrowsProdPlants SET  [Middle Level Quantity] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+	ELSE IF (@UpdateType = 'high')
+		UPDATE Property.tblLandGrowsProdPlants SET [High Level Quantity] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+	ELSE IF(@UpdateType = 'growth')
+		UPDATE Property.tblLandGrowsProdPlants SET [Growth Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+	ELSE 
+		UPDATE Property.tblLandGrowsProdPlants SET [Preservation Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Plant Name] = @Name
+END
+GO
+
+-- updates movable property table
+GO
+CREATE PROCEDURE Compensation.spUpdate2MovProp(@Name VARCHAR(23), @landID INT, @UpdateValue INT, @UpdateType VARCHAR(23))
+AS
+BEGIN
+	IF(@UpdateType = ' uproot')
+		UPDATE Property.tblMovableProperty SET [Uprooting Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Property Name] = @Name
+	ELSE IF(@UpdateType = 'transport')
+		UPDATE Property.tblMovableProperty SET  [Transportation Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Property Name] = @Name
+	ELSE IF (@UpdateType = 'install')
+		UPDATE Property.tblMovableProperty SET [Installation Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Property Name] = @Name
+	ELSE 
+		UPDATE Property.tblMovableProperty SET [Recovery Expense] = @UpdateValue WHERE [Land ID] = @landID AND [Property Name] = @Name
+END
+GO
